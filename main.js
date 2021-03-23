888b69c1c2b33f4cc864b3963f000f75f251a6eb
ijul <muhamad.info@gmail.com>
2021-03-23 09:32:56 +0700

    Update - Multiple twitter and telegram broadcast

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
-var app     = require("./run.js")
-var twitter = require("./lib/get_user_tweet_checker");
-var telegram= require("./lib/push_telegram_msg");
-var global  = require("./lib/global_functions");
+/**/
+var app             = require("./run.js")
+var twitterSource   = require("./lib/get_user_tweet_checker");
+var twitterDest     = require("./lib/post_tweet_update");
+var telegram        = require("./lib/push_telegram_msg");
+var global          = require("./lib/global_functions");
 
-const main = async (twitter,telegram) => {
+const main = async (twitterSource,telegram,twitterDest) => {
     while(true) {
         await global.delay(3000);
-        await app.run(twitter,telegram);
+        await app.run(twitterSource,telegram,twitterDest);
     }
 }
 
-console.log(new Date() + ' Initiating tweet id ');
+console.log(new Date() + ' Inisialisasi tweet_id ');
 
 global.tweet_id = global.readFile(process.env.LOCAL_FILENAME);
 
 console.log( new Date() + ' tweet id ' + global.tweet_id);
 
-let resp = main(twitter,telegram);
+let resp = main(twitterSource,telegram,twitterDest);
+
+/*============================================
+var runner = require("child_process");
+var phpScriptPath = "php_script/php_call.php";
+var argsString = "value1,value2,value3";
+runner.exec("php " + phpScriptPath + " " +argsString, function(err, phpResponse, stderr) {
+ if(err) console.log(err); // log error 
+console.log( phpResponse );
+});
+*/
--- run.js
+++ run.js
@@ -5,18 +5,22 @@
 // alternative 2 using post 
 var axios   = require('axios');
 
-function run(twitter, telegram){
-    axios(twitter.config)
+function run(twitterSource, telegram, twitterDest){
+    axios(twitterSource.config)
     .then(function (response) {
         var newest_id = response.data.meta.newest_id;
-        var ret = twitter.tweetChecker(newest_id);
+        var ret = twitterSource.tweetChecker(newest_id);
+        var msg = response.data.data[0].text
+                    + "\n\nLihat lebih detil:\n"
+                    +'https://twitter.com/'+ process.env.TWITTER_USER_ID
+                    +'/status/'
+                    +newest_id;
         if (ret){
-            console.log(new Date() + ' Sending message...');
-            telegram.sendMessage(response.data.data[0].text
-                + "\n\nLihat lebih detil:\n"
-                +'https://twitter.com/'+ process.env.TWITTER_USER_ID
-                +'/status/'
-                +newest_id);
+            console.log(new Date() + ' Mengirim pesan ke telegram...');
+            telegram.sendMessage(msg);
+
+            console.log(new Date() + ' Mengirim pesan ke twitter...');
+            twitterDest.sendUpdate(msg);
         }
     })
     .catch(function (error) {
